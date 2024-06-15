8f620653736004af956147a11480891747137c83
funczz <funczz@protonmail.com>
2024-06-15 16:39:38 +0900

    feat ReadyActivity エントリポイントを追加する

--- app/src/main/java/com/github/funczz/kotlin/rocket_launcher/android/activity/ready/ReadyActivity.kt
+++ app/src/main/java/com/github/funczz/kotlin/rocket_launcher/android/activity/ready/ReadyActivity.kt
@@ -1,46 +1,29 @@
-package com.github.funczz.kotlin.rocket_launcher.android
+package com.github.funczz.kotlin.rocket_launcher.android.activity.ready
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.github.funczz.kotlin.rocket_launcher.android.ui.theme.RocketLauncherTheme
 
-class MainActivity : ComponentActivity() {
+class ReadyActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             RocketLauncherTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxWidth(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ReadyScreen(
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RocketLauncherTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/github/funczz/kotlin/rocket_launcher/android/activity/ready/ReadyScreen.kt
@@ -0,0 +1,62 @@
+package com.github.funczz.kotlin.rocket_launcher.android.activity.ready
+
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.github.funczz.kotlin.rocket_launcher.android.R
+import com.github.funczz.kotlin.rocket_launcher.android.ui.theme.RocketLauncherTheme
+
+
+@Composable
+fun ReadyScreen(
+    modifier: Modifier = Modifier,
+) {
+    val context = LocalContext.current
+
+    val owner = LocalLifecycleOwner.current
+
+    SideEffect { Log.i("ReadyScreen", "content created.") }
+
+    Column {
+        //input field
+        BasicTextField(
+            value = "",
+            onValueChange = {
+            },
+            modifier = modifier.testTag("field"),
+        )
+
+        //start button
+        Button(
+            onClick = {
+            },
+            modifier = modifier.testTag("button"),
+            enabled = true
+        ) {
+            Text(
+                text = stringResource(id = R.string.startButton),
+            )
+        }
+    }
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun ReadyPreview() {
+    RocketLauncherTheme {
+        ReadyScreen(
+        )
+    }
+}
