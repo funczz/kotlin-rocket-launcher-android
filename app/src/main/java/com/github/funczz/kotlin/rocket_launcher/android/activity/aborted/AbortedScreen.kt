cd1a39e6b6b4cdde5b9c69bb4c0d3a1b32a05a0d
funczz <funczz@protonmail.com>
2024-06-16 14:01:21 +0900

    feat feat AbortedActivity Aborted画面を追加する

---
+++ app/src/main/java/com/github/funczz/kotlin/rocket_launcher/android/activity/aborted/AbortedScreen.kt
@@ -0,0 +1,60 @@
+package com.github.funczz.kotlin.rocket_launcher.android.activity.aborted
+
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.github.funczz.kotlin.rocket_launcher.android.R
+import com.github.funczz.kotlin.rocket_launcher.android.ui.theme.RocketLauncherTheme
+
+@Composable
+fun AbortedScreen(
+    modifier: Modifier = Modifier,
+) {
+
+    val context = LocalContext.current
+
+    val owner = LocalLifecycleOwner.current
+
+    SideEffect { Log.i("AbortedScreen", "content created") }
+
+    Column {
+        //count field
+        BasicTextField(
+            value = "N/A",
+            onValueChange = {},
+            modifier = modifier.testTag("field"),
+            readOnly = true,
+        )
+
+        //ready button
+        Button(
+            onClick = {
+            },
+            modifier = modifier.testTag("button"),
+            enabled = true,
+        ) {
+            Text(
+                text = stringResource(id = R.string.readyButton),
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AbortedPreview() {
+    RocketLauncherTheme {
+        AbortedScreen(
+        )
+    }
+}
